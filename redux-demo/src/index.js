3a0667d20bb50bdb7260004f06d02defebac61b7
KING <2419688248@qq.com>
2020-05-05 22:38:23 +0800

    Reducers学习

--- redux-demo/src/index.js
+++ redux-demo/src/index.js
@@ -1,70 +1,33 @@
-// reudx的使用
-import { createStore } from "redux"
-// action生成器
-const countIncrementBy = ({ incrementBy = 1 } = {}) => {
-  return {
-    type: "INCREMENT",
-    incrementBy
-  }
-}
-const countDecrmentBy = ({ decrmentBy = 1 } = {}) => {
-  return {
-    type: "DECREMENT",
-    decrmentBy
-  }
-}
-const countReset = () => {
-  return {
-    type: "RESET"
-  }
-}
-const countSet = ({ count = 1 } = {}) => {
-  return {
-    type: "SET",
-    count
-  }
-}
-// reudx
-const store = createStore((state = { count: 0 }, active) => {
-  switch (active.type) {
-    case "INCREMENT":
-      return {
-        count: state.count + active.incrementBy
-      }
-    case "DECREMENT":
-      return {
-        count: state.count - active.decrmentBy
-      }
-    case "RESET":
-      return {
-        count: 0
-      }
-    case "SET": {
-      return {
-        count: active.count
-      }
+import { createStore, combineReducers } from "redux"
+// AddExpense
+// RemoveExpense
+// EditExpense
+// SetTextFilter
+// SortByDate
+// SortByAmount
+// Setstartdate
+// Setenddate
+const HandleExpenseReducer = (state = [], action) => {
+    switch (action.type) {
+        default:
+            return state
     }
-    default:
-      return state
-  }
+}
+const HandleFiltersReducer = (state = {
+    text: "",
+    sortBy: "date", //date or amount
+    startDate: undefined,
+    endDate: undefined,
+}, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
 
-})
-// 监听器
-store.subscribe(() => {
-  console.log(store.getState())
-  // 一般重新渲染dom
-})
-// 更新redux
-store.dispatch(countIncrementBy({
-  incrementBy: 5
+const store = createStore(combineReducers({
+    expenses: HandleExpenseReducer,
+    filters: HandleFiltersReducer
 }))
-store.dispatch(countIncrementBy())
-store.dispatch(countDecrmentBy({
-  decrmentBy: 10
-}))
-store.dispatch(countDecrmentBy())
-store.dispatch(countReset())
-store.dispatch(countSet())
-store.dispatch(countSet({
-  count: 1000
-}))
+
+console.log(store.getState())
