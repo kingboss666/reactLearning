20b7a9a265f154a93011bb7251e4e46affe7f0f5
KING <2419688248@qq.com>
2020-05-05 20:51:26 +0800

    redux学习

---
+++ redux-demo/src/index.js
@@ -0,0 +1,70 @@
+// reudx的使用
+import { createStore } from "redux"
+// action生成器
+const countIncrementBy = ({ incrementBy = 1 } = {}) => {
+  return {
+    type: "INCREMENT",
+    incrementBy
+  }
+}
+const countDecrmentBy = ({ decrmentBy = 1 } = {}) => {
+  return {
+    type: "DECREMENT",
+    decrmentBy
+  }
+}
+const countReset = () => {
+  return {
+    type: "RESET"
+  }
+}
+const countSet = ({ count = 1 } = {}) => {
+  return {
+    type: "SET",
+    count
+  }
+}
+// reudx
+const store = createStore((state = { count: 0 }, active) => {
+  switch (active.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + active.incrementBy
+      }
+    case "DECREMENT":
+      return {
+        count: state.count - active.decrmentBy
+      }
+    case "RESET":
+      return {
+        count: 0
+      }
+    case "SET": {
+      return {
+        count: active.count
+      }
+    }
+    default:
+      return state
+  }
+
+})
+// 监听器
+store.subscribe(() => {
+  console.log(store.getState())
+  // 一般重新渲染dom
+})
+// 更新redux
+store.dispatch(countIncrementBy({
+  incrementBy: 5
+}))
+store.dispatch(countIncrementBy())
+store.dispatch(countDecrmentBy({
+  decrmentBy: 10
+}))
+store.dispatch(countDecrmentBy())
+store.dispatch(countReset())
+store.dispatch(countSet())
+store.dispatch(countSet({
+  count: 1000
+}))
