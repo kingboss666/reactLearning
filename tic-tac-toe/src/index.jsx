14b1fedeb977d22b336c8fcde017008f35629e8d
king <2419688248@qq.com>
2020-05-08 17:12:41 +0800

    react 官网学习

---
+++ tic-tac-toe/src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import "./Style/_Style.scss"
+import Game from "./components/Game"
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
